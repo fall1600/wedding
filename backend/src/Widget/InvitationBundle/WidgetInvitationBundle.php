7f384ea5793cd8fd2d0332ff5e89857b46a58c33
jhlin <jhlin@dgfactor.com.tw>
2018-10-23 13:56:58 +0800

    setting

--- backend/src/Widget/InvitationBundle/WidgetInvitationBundle.php
+++ backend/src/Widget/InvitationBundle/WidgetInvitationBundle.php
@@ -2,8 +2,9 @@
 
 namespace Widget\InvitationBundle;
 
+use Backend\BaseBundle\CM4\CM4WidgetInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class WidgetInvitationBundle extends Bundle
+class WidgetInvitationBundle extends Bundle implements CM4WidgetInterface
 {
 }
