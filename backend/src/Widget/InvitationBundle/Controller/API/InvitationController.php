5a704292e1537d4ede731847daa968125e0e44f8
jhlin <jhlin@dgfactor.com.tw>
2018-10-23 17:06:05 +0800

    test for create a invitation

--- backend/src/Widget/InvitationBundle/Controller/API/InvitationController.php
+++ backend/src/Widget/InvitationBundle/Controller/API/InvitationController.php
@@ -3,9 +3,12 @@
 namespace Widget\InvitationBundle\Controller\API;
 
 use Backend\BaseBundle\Controller\API\BaseController;
+use JMS\DiExtraBundle\Annotation as DI;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Widget\InvitationBundle\Model\InvitationQuery;
+use Widget\InvitationBundle\Service\InvitationService;
 
 /**
  * @Route("/invitation")
@@ -13,11 +16,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class InvitationController extends BaseController
 {
     /**
+     * @var InvitationService
+     * @DI\Inject()
+     */
+    protected $invitationService;
+
+    /**
      * @Route("s")
      * @Method({"POST"})
      */
     public function createAction(Request $request)
     {
+        $parameter = json_decode($request->getContent(), true);
+        $result = $this->invitationService->create($parameter);
+        return $this->createJsonSerializeResponse($result, array("detail"));
+    }
 
+    /**
+     * @Route("s")
+     * @Method({"GET"})
+     */
+    public function indexAction(Request $request)
+    {
+        $result = InvitationQuery::create()->find();
+        return $this->createJsonSerializeResponse($result, array("list"));
     }
 }
---
+++ backend/src/Widget/InvitationBundle/Tests/Controller/API/InvitationControllerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Widget\InvitationBundle\Tests\Controller\API;
+
+use Backend\BaseBundle\Tests\Fixture\BaseWebTestCase;
+use Widget\InvitationBundle\Model\InvitationQuery;
+
+class InvitationControllerTest extends BaseWebTestCase
+{
+    public function test_createAction()
+    {
+        //arrange
+        $parameter = array(
+            "name" => "魏餅餅",
+            "phone" => "0988555666"
+        );
+
+        //act
+        $this->client->request(
+            'POST',
+            $this->generateUrl('widget_invitation_api_invitation_create'),
+            array(),
+            array(),
+            array(),
+            json_encode($parameter)
+        );
+        $response = $this->client->getResponse();
+        $result = json_decode($response->getContent(), true);
+
+        //assert
+        $this->assertTrue($response->isOk());
+        $this->assertArrayHasKey("id", $result);
+        $this->assertArrayHasKey("name", $result);
+        $this->assertArrayHasKey("nick_name", $result);
+        $this->assertArrayHasKey("phone", $result);
+        $this->assertArrayHasKey("number_of_people", $result);
+        $this->assertArrayHasKey("address", $result);
+        $this->assertArrayHasKey("email", $result);
+        $this->assertArrayHasKey("attend", $result);
+        $this->assertArrayHasKey("known_from", $result);
+        $this->assertArrayHasKey("is_vegetarian", $result);
+        $this->assertArrayHasKey("baby_seat", $result);
+        $this->assertArrayHasKey("note", $result);
+        $this->assertArrayHasKey("created_at", $result);
+        $this->assertArrayHasKey("updated_at", $result);
+
+        //teardown
+        InvitationQuery::create()->findOneById($result['id'])->delete();
+    }
+}
