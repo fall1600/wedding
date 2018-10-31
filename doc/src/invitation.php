232a3dae75b7083d4f9571a82593d778df08febe
jhlin <fall1600@gmail.com>
2018-10-31 21:47:14 +0800

    arrange doc

--- doc/src/invitation.php
+++ doc/src/invitation.php
@@ -13,15 +13,17 @@
  * @apiParam {string} [email] 喜帖email (電子喜帖, 有填就寄)
  * @apiParam {string} attend 出席意願
  * <ul>
- * <li>不出席</li>
- * <li>出席</li>
- * <li>禮到人未到</li>
- * <li>尚未確定</li>
+ * <li>no (不出席)</li>
+ * <li>taipei (出席台北場)</li>
+ * <li>chiayi (出席嘉義場)</li>
+ * <li>both (出席兩場)</li>
+ * <li>blessing (禮到人未到)</li>
+ * <li>notsure (尚未確定)</li>
  * </ul>
  * @apiParam {string} known_from 男方或女方的親友
  * <ul>
- * <li>男方</li>
- * <li>女方</li>
+ * <li>male (男方)</li>
+ * <li>female (女方)</li>
  * </ul>
  * @apiParam {string} [note] 祝福的話
  * @apiParamExample 參數範例
@@ -34,8 +36,8 @@
  *    "number_of_baby_seat": 0,
  *    "address": "民族西路296號",
  *    "email": "fall1600@gmail.com",
- *    "attend": "出席",
- *    "known_from": "男方",
+ *    "attend": "taipei",
+ *    "known_from": "male",
  *    "note": "新婚快樂 <3"
  * }
  * @apiSuccessExample {json} 成功回傳範例
@@ -57,7 +59,7 @@
  *    "number_of_baby_seat": 0,
  *    "address": "民族西路296號",
  *    "email": "fall1600@gmail.com",
- *    "attend": "出席",
- *    "known_from": "男方",
+ *    "attend": "taipei",
+ *    "known_from": "male",
  *    "note": "新婚快樂 <3"
  */
