6d118a7de0d5ec25c63e9ea29e38fa0013e87857
Yu Jo Chia <zoec0114@gmail.com>
2018-11-04 23:51:53 +0800

    Add form control

--- frontend/src/app/weddingform/weddingform.component.ts
+++ frontend/src/app/weddingform/weddingform.component.ts
@@ -12,16 +12,48 @@ export class WeddingformComponent implements OnInit {
     private fb: FormBuilder,
     ) {}
 
+
   weddingForm = this.fb.group({
     fullname: ['', Validators.required],
     nickname: ['', Validators.required],
+    attendence: ['', Validators.required],
+    frends: ['', Validators.required],
+    numPeople: ['1', Validators.required],
+    numVegetarian: ['0', Validators.required],
+    numBabyseat: ['0', Validators.required],
+    phone: '',
+    address: '',
+    email: '',
+    note: ''
   });
+
+  _invitation:string = 'email' ;
+  
 
   submitForm(): void {
 
   }
 
   ngOnInit() {
+    console.log();
   }
+
+  setInvitation(invitation) {
+    this._invitation = invitation;
+    if( invitation === 'address'){
+      this.weddingForm.get('phone').reset();
+    } else if ( invitation === 'phone'){
+      this.weddingForm.get('address').reset();
+    }
+  }
+
+  isShow(invitation) {
+    if( this._invitation === 'sendBoth' || this._invitation === invitation) {
+      return true
+    } else {
+      return false
+    }
+  }
+
 
 }
