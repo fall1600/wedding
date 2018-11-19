f9c921629f39e5880b5b4dc4272aaf24d4c06349
fall1600 <fall1600@gmail.com>
2018-11-20 00:50:17 +0800

    register events

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,6 +7,23 @@ import { FormsModule, ReactiveFormsModule, Validators, FormBuilder } from '@angu
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent  {
-  
+  ngOnInit() {
+    this.registerEvents();
+  };
 
+  registerEvents() {
+    document.addEventListener("keydown", (e) => {
+      // Prevent F12
+      if (e.keyCode == 123) { 
+        e.preventDefault();
+      }
+      // Prevent Ctrl+Shift+I
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 73) { 
+        e.preventDefault();
+      }
+    });
+    document.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+    });
+  }
 }
