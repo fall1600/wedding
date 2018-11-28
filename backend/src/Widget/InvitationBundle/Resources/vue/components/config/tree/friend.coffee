7b5a60164f0970aa7aa970726e11450ab9361a67
jhlin <fall1600@gmail.com>
2018-11-28 20:20:19 +0800

    basic tree setting

---
+++ backend/src/Widget/InvitationBundle/Resources/vue/components/config/tree/friend.coffee
@@ -0,0 +1,15 @@
+module.exports =
+  levelLimit: 2
+  actions: {
+    '0': {
+      new: true
+    },
+    '1': {
+      new: true
+      update: true
+    },
+    '2': {
+      new: true
+      update: true
+    },
+  }
---
+++ backend/src/Widget/InvitationBundle/Resources/vue/components/router/tree/friend.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page-content">
+    <category :useCustomize="customize"></category>
+  </div>
+</template>
+
+<script lang="babel!coffee" type="text/coffeescript">
+  module.exports =
+    mixins: [require "components/backendbase/mixins/categoryConfig.coffee"]
+#    data: () ->
+#      customize: [
+#        require "../../customize/channel_tree/config.vue"
+#      ]
+    methods:
+      getModel: () ->
+        require('components/widgetinvitation/actions/tree/friend.coffee')
+</script>
