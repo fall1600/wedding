dc19e278b898e09f91dc92c0cdea38d86515b08c
fall1600 <fall1600@gmail.com>
2018-12-02 09:27:18 +0800

    api doc for siteconfig

---
+++ doc/src/siteconfig.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @api {GET} /config/recaptcha_site_key  recaptcha-取得
+ * @apiName GetRecaptchaKey
+ * @apiGroup SiteConfig
+ * @apiSuccessExample {json} 成功回傳範例
+ * HTTP/1.1 200 OK
+ * RecaptchaResponse
+ * @apiUse RecaptchaResponse
+ */
+
+/**
+ * @apiDefine RecaptchaResponse
+ * @apiSuccessExample Recaptcha
+ * {
+ *    "config": "fsdfasall1sf6asasdfdf0asf0",
+ * }
+ */
